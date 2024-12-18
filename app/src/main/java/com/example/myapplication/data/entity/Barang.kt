80672f7914dab5a5811c3a0640d5ef43e7222062
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-18 13:47:05 +0700

    membuat entitas barang

---
+++ app/src/main/java/com/example/myapplication/data/entity/Barang.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.data.entity
+
+data class Barang (
+    val id: String = "",
+    val nama: String = "",
+    val deskripsi: String = "",
+    val harga: String = "",
+    val stok: String = "",
+    val namasuplier: String = "",
+)
