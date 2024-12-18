c7bbdaa8a7a0c94abb39ae470e3acfe1a771100b
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-18 14:10:19 +0700

    membuat oprasi delete data barang dari table

--- app/src/main/java/com/example/myapplication/data/dao/BarangDao.kt
+++ app/src/main/java/com/example/myapplication/data/dao/BarangDao.kt
@@ -1,8 +1,12 @@
 package com.example.myapplication.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.myapplication.data.entity.Barang
+import com.example.myapplication.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -10,5 +14,16 @@ interface BarangDao {
     @Insert
     suspend fun insertBARANG(
         barang: Barang
+    )
+
+@Query("SELECT * FROM Barang  ORDER BY nama")
+fun getAllBarang() : Flow<Suplier>
+
+@Query("SELECT * FROM Barang ORDER BY deskripsi")
+fun getBarang (deskripsi: String) Flow<Barang>
+
+    @Delete
+    suspend fun deletebarang(
+       barang: Barang
     )
 }
