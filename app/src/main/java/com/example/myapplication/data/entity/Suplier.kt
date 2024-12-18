a968502ecb051dde21c82dc72c4ee728f99022c4
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-18 13:48:27 +0700

    membuat entitas suplier

---
+++ app/src/main/java/com/example/myapplication/data/entity/Suplier.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.data.entity
+
+data class Suplier (
+    val id: String = "",
+    val nama: String = "",
+    val kontak: String = "",
+    val alamat: String = "",
+)
